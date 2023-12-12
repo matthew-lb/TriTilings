a19ed68d1aa46b5dbe0174de96200c0923a6f1c4
matthew-lb <matthew0367@gmail.com>
2023-12-11 23:34:47 -0500

    Implemented Luxor Graphics

--- TriTiling_Graphics.jl
+++ TriTiling_Graphics.jl
@@ -1,9 +1,12 @@
 #################### TIKZ DRAWING ####################
 include("TriTilings.jl")
 using DelimitedFiles
+using Luxor
 using Printf
 
-function define_edimers()
+#################### TIKZ DRAWINGS ####################
+
+function tikz_edimers()
     s = "\\def\\edimer{"
     for i in 0:5
         s *= "+("
@@ -23,7 +26,7 @@ function define_edimers()
     return s
 end
 
-function define_sedimers()
+function tikz_sedimers()
     s = "\\def\\sedimer{"
     for i in 5:10
         s *= "+("
@@ -43,7 +46,7 @@ function define_sedimers()
     return s
 end
 
-function define_swdimers()
+function tikz_swdimers()
     s = "\\def\\swdimer{"
     for i in 4:9
         s *= "+("
@@ -63,12 +66,12 @@ function define_swdimers()
     return s
 end
 
-function latex_preamble(;xscale = 1.0, yscale = 1.0)
-    return "\\documentclass[12pt]{amsart}\n\\usepackage{tikz}\n\\begin{document}\n\\begin{tikzpicture}[xscale="*string(xscale)*", yscale ="*string(yscale)*"]"
+function tikz_coors(i1, j1)
+    return i1 - j1/2, -j1*sqrt(3)/2
 end
 
-function convert_coors(i1, j1)
-    return i1 - j1/2, -j1*sqrt(3)/2
+function latex_preamble(;xscale = 1.0, yscale = 1.0)
+    return "\\documentclass[12pt]{amsart}\n\\usepackage{tikz}\n\\begin{document}\n\\begin{tikzpicture}[xscale="*string(xscale)*", yscale ="*string(yscale)*"]"
 end
 
 function tikz_lattice(tiling; jump = 5, errors_only = true)
@@ -77,7 +80,7 @@ function tikz_lattice(tiling; jump = 5, errors_only = true)
     end
     s = ""
     for (i,j) in tiling.iterator_helper_array[1]
-        x,y = convert_coors(i,j)
+        x,y = tikz_coors(i,j)
         if !valid_tiling_at(tiling, i, j)
             s *= "\\filldraw[fill=red] ("*string(x)*","*string(y)*") circle (.2cm);\n"
         elseif !errors_only
@@ -88,8 +91,8 @@ function tikz_lattice(tiling; jump = 5, errors_only = true)
 end
 
 function tikz_line(i1,j1,i2,j2)
-    x1, y1 = convert_coors(i1, j1)
-    x2, y2 = convert_coors(i2, j2)
+    x1, y1 = tikz_coors(i1, j1)
+    x2, y2 = tikz_coors(i2, j2)
     start_node = "\\filldraw[fill=black] ("*string(x1)*","*string(y1)*") circle (.2cm);\n"
     end_node = "\\filldraw[fill=black] ("*string(x2)*","*string(y2)*") circle (.2cm);\n"
     line = "\\draw ("*string(x1)*","*string(y1)*") -- ("*string(x2)*","*string(y2)*");\n"
@@ -97,8 +100,8 @@ function tikz_line(i1,j1,i2,j2)
 end
 
 function tikz_dimer(i1,j1,i2,j2)
-    x1, y1 = convert_coors(i1, j1)
-    x2, y2 = convert_coors(i2, j2)
+    x1, y1 = tikz_coors(i1, j1)
+    x2, y2 = tikz_coors(i2, j2)
     if (y1 == y2) && (x1 < x2)
         return "\\filldraw[fill=blue!20] ("*string(x1)*","*string(y1)*") \\edimer;"
     elseif (y1 == y2) && (x2 < x1)
@@ -114,13 +117,13 @@ function tikz_dimer(i1,j1,i2,j2)
     end
 end
 
-function save_matching_to_file(tiling, tikz_func, filename; xscale = 1.0, yscale = 1.0, jump = 5)
+function save_matching_to_tex_file(tiling, tikz_func, filename; xscale = 1.0, yscale = 1.0, jump = 5)
     file_text = []
     open(filename, "w") do io
         write(io, latex_preamble(xscale = xscale, yscale = yscale))
-        write(io, define_edimers()*"\n")
-        write(io, define_sedimers()*"\n")
-        write(io, define_swdimers()*"\n")
+        write(io, tikz_edimers()*"\n")
+        write(io, tikz_sedimers()*"\n")
+        write(io, tikz_swdimers()*"\n")
         l,w = tiling.domain_dimensions
         for (i,j) in tiling.iterator_helper_array[1]
             if get_up(tiling,i,j) == 1
@@ -140,5 +143,108 @@ function save_matching_to_file(tiling, tikz_func, filename; xscale = 1.0, yscale
         end
         write(io, tikz_lattice(tiling, jump = jump))
         write(io,"\\end{tikzpicture}\n\\end{document}")
+    end
+end
+
+#################### LUXOR DRAWINGS ####################
+
+function luxor_coors(i,j)
+    return i - j/2, j*sqrt(3)/2
+end
+
+function luxor_hexagon()
+    move(Point(cos(2*pi*(.5)/6)/sqrt(3), sin(2*pi*(.5)/6)/sqrt(3)))
+    for i in 1:5
+        line(Point(cos(2*pi*(i+.5)/6)/sqrt(3), sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    closepath()
+    fillstroke()
+end
+
+function luxor_edimer(x,y;col ="aqua")
+    Luxor.translate(Point(x,y))
+    setcolor(col)
+    setline(0)
+    move(Point(cos(2*pi*(.5)/6)/sqrt(3), -sin(2*pi*(.5)/6)/sqrt(3)))
+    for i in 1:5
+        line(Point(cos(2*pi*(i+.5)/6)/sqrt(3), -sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    for i in 3:7
+        line(Point(1+cos(2*pi*(i+.5)/6)/sqrt(3), -sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    closepath()
+    fillstroke()
+end
+
+function luxor_sedimer(x,y; col = "aquamarine2")
+    Luxor.translate(Point(x,y))
+    setcolor(col)
+    setline(0)
+    move(Point(cos(2*pi*(5.5)/6)/sqrt(3), -sin(2*pi*(5.5)/6)/sqrt(3)))
+    for i in 6:10
+        line(Point(cos(2*pi*(i+.5)/6)/sqrt(3), -sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    for i in 2:6
+        line(Point(1/2+cos(2*pi*(i+.5)/6)/sqrt(3), sqrt(3)/2-sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    closepath()
+    fillstroke()
+end
+
+function luxor_swdimer(x,y; col = "blue")
+    Luxor.translate(Point(x,y))
+    setcolor(col)
+    setline(0)
+    move(Point(cos(2*pi*(4.5)/6)/sqrt(3), -sin(2*pi*(4.5)/6)/sqrt(3)))
+    for i in 5:9
+        line(Point(cos(2*pi*(i+.5)/6)/sqrt(3), -sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    for i in 1:5
+        line(Point(-1/2+cos(2*pi*(i+.5)/6)/sqrt(3), sqrt(3)/2-sin(2*pi*(i+.5)/6)/sqrt(3)))
+    end
+    closepath()
+    fillstroke()
+end
+
+function luxor_dimer(i1,j1,i2,j2; e_col = "lightblue", se_col = "blue", sw_col = "darkblue")
+    x1, y1 = luxor_coors(i1, j1)
+    x2, y2 = luxor_coors(i2, j2)
+    if (y1 == y2) && (x1 < x2)
+        luxor_edimer(x1, y1, col = e_col)
+    elseif (y1 == y2) && (x2 < x1)
+        luxor_edimer(x2, y2, col = e_col)
+    elseif (x1 < x2) && (y1 < y2)
+        luxor_sedimer(x1, y1, col = se_col)
+    elseif (x1 > x2) && (y1 > y2)
+        luxor_sedimer(x2, y2, col = se_col)
+    elseif (x1 > x2) && (y1 < y2)
+        luxor_swdimer(x1, y1, col = sw_col)
+    else
+        luxor_swdimer(x2, y2, col = sw_col)
+    end
+end
+
+function save_matching_to_luxor_file(tiling, luxor_func)
+    @svg begin
+        for (i,j) in tiling.iterator_helper_array[1]
+            gsave()
+            if get_up(tiling,i,j) == 1
+                luxor_func(i,j,i+1,j+1)
+            elseif get_up(tiling,i,j) == 2
+                luxor_func(i,j+1,i+1,j+1)
+            elseif get_up(tiling,i,j) == 3
+                luxor_func(i,j,i,j+1)
+            end
+            grestore()
+            gsave()
+            if get_down(tiling,i,j) == 1
+                luxor_func(i,j,i+1,j+1)
+            elseif get_down(tiling,i,j) == 2
+                luxor_func(i,j,i+1,j)
+            elseif get_down(tiling,i,j) == 3
+                luxor_func(i+1,j,i+1,j+1)
+            end
+            grestore()
+        end
     end
 end
